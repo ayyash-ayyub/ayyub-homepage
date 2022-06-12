85ea632282c2a5a2b2576b4a85f6b93864b1adb8
AYYUB <rizal.it@gmail.com>
2022-06-12 17:34:13 +0700

    test deploy parcel

--- pages/index.js
+++ pages/index.js
@@ -79,12 +79,7 @@ const Home = () => (
           Work
         </Heading>
         <Paragraph>
-          Takuya is a freelance and a full-stack developer based in Osaka with a
-          passion for building digital services/stuff he wants. He has a knack
-          for all things launching products, from planning and designing all the
-          way to solving real-life problems with code. When not online, he loves
-          hanging out with his camera. Currently, he is living off of his own
-          product called{' '}
+          Abdul Rizal Adompo bla bla bla{' '}
           <NextLink href="/works/inkdrop" passHref scroll={false}>
             <Link>Inkdrop</Link>
           </NextLink>
