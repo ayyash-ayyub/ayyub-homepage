ca825f48b30b43e8d3affc13849b58dec44ed2dd
AYYUB <rizal.it@gmail.com>
2022-06-21 10:24:42 +0700

    add meta google

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -16,19 +16,19 @@ const Main = ({ children, router }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Ayyub's homepage" />
-        <meta name="author" content="Ayyub Adompo" />
-        <meta name="author" content="craftzdog" />
+        <meta name="author" content="Abdul Rizal Adompo" />
+        <meta name="author" content="adompo" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        {/* <meta name="twitter:title" content="Abdul Rizal ADompo" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" /> */}
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta name="twitter:title" content="Abdul Rizal Adompo" />
+
+        <meta name="google-site-verification" content="HgHkAMDBOJxld7Zb3hBYFF1esGk5hZWUtOQNIy6SuoY" />
+  
+        {/* <meta name="twitter:image" content="https://www.craftz.dog/card.png" /> */}
         <meta property="og:site_name" content="Ayyub Adompo" />
         <meta name="og:title" content="Ayyub Adompo" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        {/* <meta property="og:image" content="https://www.craftz.dog/card.png" /> */}
         <title>Ayyub Adompo - Homepage</title>
       </Head>
 
